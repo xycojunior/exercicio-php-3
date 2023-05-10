757ed510a61a4eebce895b1325c0cf32cbe33942
Daniel Tavares Almeida Correia <daniel.tavares@aluno.unifapce.edu.br>
2023-05-10 17:07:42 -0300

    Update beecrowd-1542.php

--- 1542 - Lendo Livros/beecrowd-1542.php
+++ 1542 - Lendo Livros/beecrowd-1542.php
@@ -10,11 +10,11 @@ while (1) {
     $d = $input[1]; #dias economizados se ler pags por dia igual a P
     $p = $input[2];
 
-    $a = ($q * $d); #mult entre "pags por dia" e "dias economizados" 48
-    $b = ($p - $q); #sub entre "X pags por dia" e "dias economizados" 2
+    $pxd = ($q * $d); #mult entre "pags por dia" e "dias economizados" 48
+    $psd = ($p - $q); #sub entre "X pags por dia" e "dias economizados" 2
 
-    $t = $a / $b; #divisao entre os resultados acima
-    $paginas = $t * $p;
+    $div = $pxd / $psd; #divisao entre os resultados acima
+    $paginas = $div * $p;
     $paginas = floor($paginas);
 
     if ($paginas == 1) {
